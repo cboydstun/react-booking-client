a17172240d6f8021899c26ac74b0394a36d16f86
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-07-13 08:06:29 -0500

    added components
    password input bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+
+import Register from "./components/Register";
+import AdminRegister from "./components/AdminRegister";
+import Loginpage from "./components/Loginpage";
+import AdminLogin from "./components/AdminLogin";
+import Dashboard from "./components/Dashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import NewBooking from "./components/NewBooking";
+import MyBooking from "./components/MyBooking";
+import AllBooking from "./components/AllBooking";
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
-    return (
-        <div>
-            hey from app
-        </div>
-    )
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Register}/>
+          <Route path='/adminregister' component={AdminRegister}/>
+          <Route path='/login' component={Loginpage}/>
+          <Route path='/AdminLogin' component={AdminLogin}/>
+          <Route path='/dashboard' component={Dashboard}/>
+          <Route path='/admindashboard' component={AdminDashboard}/>
+          <Route path='/newbooking' component={NewBooking}/>
+          <Route path='/mybooking' component={MyBooking}/>
+          <Route path='/allbooking' component={AllBooking}/>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
